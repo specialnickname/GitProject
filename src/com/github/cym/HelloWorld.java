759e0ff31be844327302a49fbcba7490c01e25d6
cp_1989 <specialnickname@163.com>
2016-09-25 11:28:25 +0800

    git上修改
    git上修改详情

--- src/com/github/cym/HelloWorld.java
+++ src/com/github/cym/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 	public static int a;
         public static void main(String args[]){
         System.out.println("Hello World!");
-        System.out.println("ÄãºÃ");
+        System.out.println("");
 }
 }
